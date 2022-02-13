37a39ee554d0854482aedf5fd517744c42cfa430
Peizhao Li <peizhaol@linux-27.andrew.cmu.edu>
2022-02-12 21:50:01 -0500

    handling access_mode from OpenOption

--- mycode/Proxy.java
+++ mycode/Proxy.java
@@ -1,12 +1,36 @@
 /* Sample skeleton for proxy */
 
 import java.io.*;
+import java.util.Map;
 
 class Proxy {
+
+    /**
+     * map between fd and RandomAccessFile Object
+     */
+	// private Map<> fd_file_map = new HashMap<Integer, File>();
 	
 	private static class FileHandler implements FileHandling {
 
-		public int open( String path, OpenOption o ) {
+		public synchronized int open( String path, OpenOption o ) {
+			// Invalid path argument
+			if (path == null) {
+				return Errors.EINVAL;
+			}
+			String access_mode;
+			if (o == CREATE) {
+				access_mode = "rw"
+			} else if (o == CREATE_NEW) {
+				access_mode = "rw"
+			} else if (o == READ) {
+				access_mode = "r";
+			} else if (o == WRITE) {
+				access_mode = "rw";
+			}
+
+
+
+			RandomAccessFile raf = new RandomAccessFile(new File(path), o);
 			return Errors.ENOSYS;
 		}
 
@@ -19,6 +43,8 @@ class Proxy {
 		}
 
 		public long read( int fd, byte[] buf ) {
+			System.out.println("read");
+
 			return Errors.ENOSYS;
 		}
 
---
+++ mycode/set_config.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=$PWD:$PWD/../lib
+export proxyport15440=10908
+export pin15440=123987000

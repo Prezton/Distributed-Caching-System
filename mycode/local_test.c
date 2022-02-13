4d3882936f050313577a3cf94fe61a07297d8397
Peizhao Li <peizhaol@linux-40.andrew.cmu.edu>
2022-02-13 12:05:39 -0500

    adjusted local_test

--- mycode/local_test.c
+++ mycode/local_test.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 void main() {
     char *path = "./foo";
-    int fd = open(path, 0);
+    int fd = open(path, O_RDWR);
+    char *buf = malloc(sizeof(5));
+    char *test = "test2";
     printf("test open %d\n", fd);
+    printf("test write %ld\n", write(fd, test, strlen(test)));
+    printf("test read %ld\n", read(fd, buf, 5));
+    printf("things read %s\n", buf);
     printf("test close %d\n", close(fd));
 
 }
